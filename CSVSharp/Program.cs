dec5ca0a24a71afdfbc2ef8ea3eea48efd160e2e
Joel Obando <j-obando@live.com>
2014-08-04 09:15:00 -0400

    Setup the project.

---
+++ CSVSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSVSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
